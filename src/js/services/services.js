16292d7ef26717595fa998beaab24353833c8e2f
Danielanunez <naninunez0@gmail.com>
2026-01-22 13:25:11 -0300

    se crea jason con servicios

---
+++ src/js/services/services.js
@@ -0,0 +1,52 @@
+export const services = [
+    {
+        name: "Anestesia",
+        value:"Anestesia"
+
+    },
+    {
+        name: "Banco de Leche",
+        value:"Banco de Leche"
+
+    },
+    {
+        name: "Cirugia",
+        value:"Cirugia"
+
+    },
+    {
+        name: "Cirugia Plastica",
+        value:"Cirugia Plastica"
+
+    },
+    {
+        name: "Clinica Medica",
+        value:"Clinica Medica"
+
+    },
+    {
+        name: "Ecocardiograma",
+        value:"Ecocardiograma"
+
+    },
+    {
+        name: "",
+        value:""
+
+    },
+    {
+        name: "",
+        value:""
+
+    },
+    {
+        name: "",
+        value:""
+
+    },
+    {
+        name: "",
+        value:""
+
+    },
+]
